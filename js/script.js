87704d423db13ae0becea52f96998cc23426034c
Michel <richard.villalba.mendieta@gmail.com>
2023-03-03 08:04:17 -0400

    agregando modals

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ var myCarousel = document.querySelector('#carousel')
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   wrap: false
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
